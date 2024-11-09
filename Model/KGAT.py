e59e774ce0553dec27a5f875f87a342b394851b5
SonRobo <143880885+SonRobo@users.noreply.github.com>
2024-11-09 19:59:21 +0700

    Update KGAT.py

--- Model/KGAT.py
+++ Model/KGAT.py
@@ -115,7 +115,7 @@ class KGAT(object):
     def _build_weights(self):
         all_weights = dict()
 
-        initializer = tf.contrib.layers.xavier_initializer()
+        initializer = tf.keras.initializers.GlorotUniform()
 
         if self.pretrain_data is None:
             all_weights['user_embed'] = tf.Variable(initializer([self.n_users, self.emb_dim]), name='user_embed')
